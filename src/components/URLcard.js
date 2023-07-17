8884b8cef87e644e6f5379c20b4e19e1cab61c69
sagarnanera <sagarnanera30@gmail.com>
2023-07-17 11:51:17 +0530

    updated UI

--- src/components/URLcard.js
+++ src/components/URLcard.js
@@ -1,32 +1,7 @@
-// import React from 'react'
-// import ShareURL from './ShareURL'
-// import formatDate from '../utils/dateFormator'
-// import { Link } from 'react-router-dom'
-
-// const URLcard = ({ url }) => {
-
-
-//     return (
-//         <>
-//             {console.log(url)}
-//             <div className="bg-white m-2 md:m-4 rounded-lg shadow-lg p-2 md:p-6 w-full md:w-[44%]">
-//                 <Link target='_blank' to={url.Shorten_URL} className="text-lg sm:text-xl font-semibold mb-2 md:pl-4 w-[85%] md:w-auto">{url.Shorten_URL}</Link>
-//                 <p className="text-gray-600 mb-2 md:pl-4 text-sm sm:text-lg w-[85%] md:w-auto truncate">{url.Original_URL}</p>
-//                 <p className="text-gray-500 text-sm mb-4 md:pl-4 w-[85%] md:w-auto truncate">Created on {formatDate(url.createdOn)}
-//                 </p>
-//                 <ShareURL Shorten_URL={url.Shorten_URL} />
-//             </div>
-//         </>
-//     )
-// }
-
-// export default URLcard
-
 import React from 'react';
 import { AiOutlineEye, AiOutlineUser, AiOutlineRobot } from 'react-icons/ai';
 import ShareURL from './ShareURL';
 import formatDate from '../utils/dateFormator';
-import { Link } from 'react-router-dom';
 import { Tooltip } from 'react-tooltip';
 
 const URLcard = ({ url, clickData }) => {
@@ -36,7 +11,7 @@ const URLcard = ({ url, clickData }) => {
     return (
         <>
             <div className="bg-white m-2 md:mx-4 md:my-2 rounded-lg shadow-lg p-2 md:px-6 md:py-2 w-full md:w-[44%]">
-                <p onClick={() => window.open(Shorten_URL, '_blank')} className="text-xl md:text-2xl font-semibold mb-2 md:pl-4 w-[98%] md:w-auto truncate">
+                <p onClick={() => window.open(Shorten_URL, '_blank')} className="cursor-pointer text-xl md:text-2xl font-semibold mb-2 md:pl-4 w-[98%] md:w-auto truncate">
                     {Shorten_URL}
                 </p>
                 <p className="text-gray-600 mb-2 md:pl-4 text-lg md:text-xl w-[85%] md:w-auto truncate">{Original_URL}</p>
